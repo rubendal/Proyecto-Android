2d87076359963e1efec8595a38e01958c8748516
Mike1nk <mikkou95@gmail.com>
2016-02-23 18:58:46 -0600

    App Paciente
    No one was born to be a master

---
+++ AppPaciente/app/src/main/java/com/example/mike/apppaciente/db/Definition.java
@@ -0,0 +1,17 @@
+package com.example.mike.apppaciente.db;
+
+import android.provider.BaseColumns;
+
+public final class Definition {
+    public Definition(){}
+
+    public static abstract class Entry implements BaseColumns{
+        public static final String NAME = "register";
+        public static final String ENTRY_ID = "id";
+        public static final String ENTRY_TIMESTAMP = "timestamp";
+        public static final String ENTRY_VALUE = "value";
+        public static final String ENTRY_PASOS = "pasos";
+
+    }
+
+}
