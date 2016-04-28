5bd8d2e36790010d48d0f4a16da364e934d04cf3
Mike1nk <mikkou95@gmail.com>
2016-04-28 16:43:34 -0500

    MainActivity / Gráficas
    Se quita el intent a GraphActivity para que MainActivity muestre la
    gráfica

--- AppPaciente/app/src/main/java/com/example/mike/apppaciente/MainActivity.java
+++ AppPaciente/app/src/main/java/com/example/mike/apppaciente/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,23 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         Paciente.paciente = new Paciente(sharedPreferences.getString("nombre",""),sharedPreferences.getString("tel",""),sharedPreferences.getInt("edad",0),sharedPreferences.getInt("genero",0),
                 sharedPreferences.getString("nombre_emergencia",""),sharedPreferences.getString("telefono_emergencia",""));
-        Paciente.paciente.setId(sharedPreferences.getInt("id",0));
+        Paciente.paciente.setId(sharedPreferences.getInt("id", 0));
+
+        TextView bienvenido = (TextView) findViewById(R.id.bienvenido);
+        bienvenido.setText("Bienvenido, " + Paciente.paciente.getNombre());
+
+        Graph.showGraph(this, R.id.graph2);
+
     }
 
-    public void activityGraficas(View view){
-        startActivity(new Intent(MainActivity.this, GraficasActivity.class));
-    }
 
     public void activityEmergencia(View view){
 
         if(Paciente.paciente != null) {
-            if(!Paciente.paciente.getTelefono_emergencia().isEmpty()) {
+            if(!Paciente.paciente.getTelefono().isEmpty()) {
                 Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + Paciente.paciente.getTelefono()));
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
                     startActivity(i);
+                } else {
+                    System.out.println("No hay permiso");
                 }
-            }
-        }
+            } else { System.out.println("No hay telefono de paciente");}
+        } else { System.out.println("Es null"); }
         //startActivity(new Intent(MainActivity.this, EmergenciaActivity.class));
     }
 
